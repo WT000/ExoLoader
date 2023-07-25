79bd0ea41ab014bd2f928aa4116d3294b286768e
Pandemonium14 <104149087+Pandemonium14@users.noreply.github.com>
2023-07-25 16:52:58 +0200

    Ajoutez des fichiers projet.

---
+++ CharacterLoader/Injector.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CharacterLoader
+{
+    [BepInEx.BepInPlugin("CharacterLoaderInject","Character Loader","0.0.1")]
+    public class Injector : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Logger.LogInfo("Doing CharacterLoader patches...");
+            var harmony = new Harmony("CharacterLoader");
+            harmony.PatchAll();
+            Logger.LogInfo("CharacterLoader patches done.");
+        }
+    }
+}
