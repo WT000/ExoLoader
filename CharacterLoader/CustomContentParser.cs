1fc0370db0c73f5a42305bff547f1d3c64f584e4
Pandemonium14 <104149087+Pandemonium14@users.noreply.github.com>
2024-02-19 18:39:48 +0100

    Progress on Custom cards

--- CharacterLoader/CustomContentParser.cs
+++ CharacterLoader/CustomContentParser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -30,8 +31,59 @@ namespace CharacterLoader
                             }
                             break;
                         }
+                    case "Cards":
+                        {
+                            ModInstance.log("Parsing cards folder");
+                            foreach(string file in Directory.GetFiles(folder))
+                            {
+                                if (file.EndsWith(".json"))
+                                {
+                                    ModInstance.log("Parsing file : " + Path.GetFileName(file));
+                                    ParseCardData(file);
+                                }
+                            }
+
+                            break;
+                        }
                 }
             }
+        }
+
+        private static void ParseCardData(string file)
+        {
+            string fullJson = File.ReadAllText(file);
+            if (fullJson == null || fullJson.Length == 0)
+            {
+                ModInstance.instance.Log("Couldn't read text for " + Path.GetFileName(file));
+            }
+            Dictionary<string, object> data = JsonConvert.DeserializeObject<Dictionary<string, object>>(fullJson);
+            if (data == null || data.Count == 0)
+            {
+                ModInstance.instance.Log("Couldn't parse json for " + Path.GetFileName(file));
+            }
+
+            CustomCardData cardData = new CustomCardData();
+
+            if (data.TryGetValue("ID", out object id))
+            {
+                cardData.id = (string)id;
+            }
+            else
+            {
+                ModInstance.instance.Log("no ID entry for " + Path.GetFileName(file));
+            }
+            ModInstance.log("Read ID entry");
+
+            if (data.TryGetValue("Name", out object name))
+            {
+                cardData.name = (string)name;
+            }
+            else
+            {
+                ModInstance.instance.Log("no Name entry for " + Path.GetFileName(file));
+            }
+            ModInstance.log("Read Name entry");
+
         }
     }
 }
