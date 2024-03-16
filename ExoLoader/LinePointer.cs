06f763a99aaf71f4af5e52de99af4164c5ee32ea
Pandemonium14 <104149087+Pandemonium14@users.noreply.github.com>
2024-03-16 11:34:32 +0100

    Added LinePointer class

---
+++ ExoLoader/LinePointer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoLoader
+{
+    public class LinePointer
+    {
+        private int currentPos;
+
+        public int GetCurrent()
+        {
+            return currentPos;
+        }
+
+        public int Next()
+        {
+            currentPos++;
+            return currentPos;
+        }
+
+        public bool SkipUntilMatch(string[] lines, string match)
+        {
+            for (int i = currentPos;  i < lines.Length; i++)
+            {
+                if (lines[i].StartsWith(match))
+                {
+                    return true;
+                }
+            }
+            ModInstance.log("SkipUntilMatch never stopped");
+            return false;
+        }
+    }
+}
